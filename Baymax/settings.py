e56a4c3b8147f2c445d6b8fa4a520608044b83ee
daniel.liu <daniel.liu@derbysofot.com>
2017-11-10 18:15:45 +0800

    change

--- Baymax/settings.py
+++ Baymax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#lo8o*9rcmimrj6-z(j%)uqzon#7_3t7r$92f79178)pue*ygw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','10.200.106.60']
 
 
 # Application definition
