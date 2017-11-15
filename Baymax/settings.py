904fac29157eec9d42d1fe700f51ee36bcc90d07
daniel.liu <daniel.liu@derbysofot.com>
2017-11-15 10:47:10 +0800

    update robot close

--- Baymax/settings.py
+++ Baymax/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#lo8o*9rcmimrj6-z(j%)uqzon#7_3t7r$92f79178)pue*ygw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','10.200.106.60']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
